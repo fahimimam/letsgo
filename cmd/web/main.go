b6c631c03b508708c8227dccee137808e5f5067e
Kazi Fahim Imam <kazi.fahim@pathao.com>
2024-11-19 17:05:30 +0600

    Add port address from three different sources successfully

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,15 +2,34 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"github.com/fahimimam/letsgo/cmd/web/handlers"
+	"github.com/joho/godotenv"
 	"log"
 	"net/http"
+	"os"
 )
 
-const PORT = ":3000"
+const PORT = "3000"
+
+type config struct {
+	addr      string
+	staticDir string
+}
+
+var cfg config
 
 func main() {
-	addr := flag.String("addr", PORT, "HTTP network Address. (Default is set to 3000)")
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+	cfg = config{
+		addr:      os.Getenv("STATIC_DIR"),
+		staticDir: os.Getenv("ADDR"),
+	}
+	flag.StringVar(&cfg.addr, "addr", cfg.addr, "HTTP network address")
+	flag.StringVar(&cfg.staticDir, "static-dir", cfg.staticDir, "Static directory")
 	flag.Parse()
 
 	mux := http.NewServeMux()
@@ -22,7 +41,13 @@ func main() {
 	mux.HandleFunc("/snippet/view", handlers.SnippetView)
 	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
 
-	log.Printf("Starting server on %v\n", *addr)
-	err := http.ListenAndServe(*addr, mux)
-	log.Fatal("Couldn't start Server: ", err)
+	if cfg.addr == "" {
+		cfg.addr = PORT
+	}
+	log.Printf("Starting server on %v\n", cfg.addr)
+	err = http.ListenAndServe(fmt.Sprintf(":%v", cfg.addr), mux)
+	if err != nil {
+		log.Fatal("Couldn't start Server: ", err)
+	}
+
 }
