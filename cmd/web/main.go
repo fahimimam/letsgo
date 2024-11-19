c1bd5860733219ec2950316d2fcfd021537ab327
Kazi Fahim Imam <kazi.fahim@pathao.com>
2024-11-19 18:32:42 +0600

    Add Dependency injection to the handlers with closure.

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/fahimimam/letsgo/cmd/web/config"
 	"github.com/fahimimam/letsgo/cmd/web/handlers"
 	"github.com/joho/godotenv"
 	"log"
@@ -12,19 +13,20 @@ import (
 
 const PORT = "3000"
 
-type config struct {
+type serverConfig struct {
 	addr      string
 	staticDir string
 }
 
-var cfg config
+var cfg serverConfig
 
 func main() {
+	// Configure Environment variables for the project.
 	err := godotenv.Load(".env")
 	if err != nil {
 		log.Println("Error loading .env file")
 	}
-	cfg = config{
+	cfg = serverConfig{
 		addr:      os.Getenv("STATIC_DIR"),
 		staticDir: os.Getenv("ADDR"),
 	}
@@ -32,22 +34,35 @@ func main() {
 	flag.StringVar(&cfg.staticDir, "static-dir", cfg.staticDir, "Static directory")
 	flag.Parse()
 
+	// Configure Custom Logger for the project.
+	infoLog := log.New(os.Stdout, "[INFO]\t", log.LstdFlags)
+	errorLog := log.New(os.Stderr, "[ERROR]\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	// Initialize app for dependency injection - (For now it's only the logger)
+	app := &config.App{
+		ErrorLog: errorLog,
+		InfoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static"))
 	mux.Handle("/static/", http.StripPrefix("/static/", fileServer))
 
-	mux.HandleFunc("/", handlers.Home)
-	mux.HandleFunc("/snippet/view", handlers.SnippetView)
-	mux.HandleFunc("/snippet/create", handlers.SnippetCreate)
+	mux.Handle("/", handlers.HomeHandler(app))
+	mux.Handle("/snippet/view", handlers.SnippetViewHandler(app))
+	mux.Handle("/snippet/create", handlers.SnippetCreateHandler(app))
 
 	if cfg.addr == "" {
 		cfg.addr = PORT
 	}
-	log.Printf("Starting server on %v\n", cfg.addr)
-	err = http.ListenAndServe(fmt.Sprintf(":%v", cfg.addr), mux)
-	if err != nil {
-		log.Fatal("Couldn't start Server: ", err)
+	infoLog.Printf("Starting server on %v\n", cfg.addr)
+	srv := http.Server{
+		Addr:     fmt.Sprintf(":%v", cfg.addr),
+		Handler:  mux,
+		ErrorLog: errorLog,
 	}
+	err = srv.ListenAndServe()
+	errorLog.Fatal("Couldn't start Server: ", err)
 
 }
